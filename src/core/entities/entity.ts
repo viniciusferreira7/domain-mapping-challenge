79c04b3a114095838f4cdd9e0c36ba88295a7f42
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-01-14 11:38:26 -0300

    feat: creates base class to extends

---
+++ src/core/entities/entity.ts
@@ -0,0 +1,15 @@
+import { UniqueEntityId } from './value-object/unique-entity-id';
+
+export class Entity<Props> {
+  private _id: UniqueEntityId
+  protected props: Props
+
+  get id(){
+    return this._id
+  }
+
+  constructor(props: Props, id?: UniqueEntityId){
+    this.props = props
+    this._id = id ?? new UniqueEntityId()
+  }
+}
